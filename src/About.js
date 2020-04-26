93fc5f2b6966e6c8cad2bc39554be7c05fcfcaa0
Jeon <jbsgo87@gmail.com>
2020-04-26 23:54:31 +0900

    install router/ reactbootstrab / styled_componets

---
+++ src/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const About = () => (
+  <div>
+    <h2>About Home</h2>
+  </div>
+)
---
+++ src/NoMatch.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const NoMatch = () => (
+  <div>
+    <h2>NoMatch Home</h2>
+  </div>
+)
